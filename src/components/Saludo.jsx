fb816b9b0ac115667be542e3f30034257413559f
Seba Flomenbaun <158225487+duncann555@users.noreply.github.com>
2025-08-24 09:18:58 -0300

    se realizo ejercicio 3

--- src/components/Saludo.jsx
+++ src/components/Saludo.jsx
@@ -1,9 +1,21 @@
-const Saludo = (props) => {
+import { useState } from "react";
+
+function Saludo(props) {
+  const [msj, setMsj] = useState("");
+
+  const mostrarMensaje = () => {
+    setMsj("(from changed state)");
+  };
+
   return (
     <div>
-      <h1>Hello World, {props.nombre}</h1>
+      <h1>
+        Hello World, {props.nombre} {msj}
+      </h1>
+      <button onClick={mostrarMensaje}>Click Aqui</button>
+
     </div>
   );
-};
+}
 
 export default Saludo;
